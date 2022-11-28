16e0a0d2f7f8485a40eb6067accdf124ad9e9f7d
ab-sid <absid24@gmail.com>
2022-11-28 12:28:26 +0600

    add product collection added

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 //middlewears
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iikigzo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const mobileCollection = client.db('mobileStore').collection('mobiles');
+        const phoneCollection = client.db('mobileStore').collection('phones');
+
+        app.get('/mobileBrands', async (req, res) => {
+            const query = {}
+            const cursor = mobileCollection.find(query);
+            const mobileBrands = await cursor.toArray();
+            res.send(mobileBrands);
+        })
+
+        app.get('/mobileBrands/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const mobileBrand = await mobileCollection.findOne(query);
+            res.send(mobileBrand);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
 
 
 app.get('/', (req, res) => {
