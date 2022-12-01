76d5482442cf0b0a0c9a218b2770046d46bbf4e8
ab-sid <absid24@gmail.com>
2022-12-01 12:17:32 +0600

    delete user added

--- index.js
+++ index.js
@@ -141,6 +141,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(filter);
+            res.send(result);
+        })
+
     }
     finally {
 
